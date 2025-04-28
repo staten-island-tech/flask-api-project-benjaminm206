23f8f8f556d9bc42f0494197e3f9f3c9f15b253e
benjaminm206 <benjaminm206@nycstudents.net>
2025-04-28 09:25:54 -0400

    4-28

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -13,30 +14,37 @@ def index():
 @app.route("/car/<int:id>")
 def car_detail(id):
     # We get detailed info for a specific Pokémon using its id.
-    response = requests.get(f"https://github.com/arthurkao/car-data{id}")
+    response = requests.get(f"https://www.carqueryapi.com/api/0.3/?cmd=getModel&model={id}")
+    
     data = response.json()
     
+    if data and 'models' in data:
+        car = data['models'][0]
     # We extract extra details like types, height, weight, and stats.
-    types = [t['type']['name'] for t in data['types']]
-    model = data.get('model')
-    make = data.get('make')
-    brand = data.get('brand').capitalize()
-    image_url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png"
+        model = data.get('model')
+        make = data.get('make')
+        brand = data.get('brand').capitalize()
+        year = data.get('year')
+        engine = data.get('engine')
+        horsepower = data.get('horsepower')
+        weight = data.get('weight')
+        image_url = f"https://raw.githubusercontent.com/arthurkao/car-images/main/{id}.png"
     
     # Get the Pokémon’s base stats (like hp, attack, defense, etc.)
     stat_names = [stat['stat']['name'] for stat in data['stats']]
     stat_values = [stat['base_stat'] for stat in data['stats']]
     
     # We tell Flask to show the 'pokemon.html' page with all these details.
-    return render_template("pokemon.html", pokemon={
-        'name': name,
+    return render_template("car.html", car={
         'id': id,
-        'image': image_url,
-        'types': types,
-        'height': height,
+        'model': model,
+        'make': make,
+        'brand': brand,
+        'year': year,
+        'engine': engine,
+        'horsepower': horsepower,
         'weight': weight,
-        'stat_names': stat_names,
-        'stat_values': stat_values
+        'image': image_url
     })
 
 if __name__ == '__main__':
