1e8322bbcfb009701e8a23e532808f0d8e2f5b4a
benjaminm206 <benjaminm206@nycstudents.net>
2025-05-05 09:26:52 -0400

    5-5

--- app.py
+++ app.py
@@ -5,43 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Get all BMW cars
-    response = requests.get("https://carapi.app/api/models?make=BMW")
-    if response.status_code == 200:
-        cars = response.json().get("data", [])
-    else:
-        cars = []
-    return render_template("index.html", cars=cars)
+    # Get all trivia questions
+    response = requests.get("https://opentdb.com/api.php?amount=10")
+    data = response.json()
 
-@app.route("/car/<int:id>")
-def car_detail(id):
-    # Get details for a specific model
-    response = requests.get("https://carapi.app/api/models?make=BMW")  # Just an example URL
-    if response.status_code == 200:
-        car = response.json()
-    else:
-        car = {}
-
-    # Dummy stats, since the API doesn't provide stats like Pokémon
-    stat_names = ["Horsepower", "Weight"]
-    stat_values = [
-        car.get("horsepower", 0),
-        car.get("weight", 0)
-    ]
-
-    return render_template("car.html", car={
-        'id': car.get("id", id),
-        'model': car.get("model_name", "Unknown"),
-        'make': car.get("make_display", "Unknown"),
-        'brand': car.get("make_display", "Unknown"),
-        'year': car.get("year", "Unknown"),
-        'engine': car.get("engine_type", "Unknown"),
-        'horsepower': car.get("horsepower", 0),
-        'weight': car.get("weight", 0),
-        'image': f"https://raw.githubusercontent.com/arthurkao/car-images/main/{id}.png",
-        'stat_names': stat_names,
-        'stat_values': stat_values
-    })
+#trivia format needs to be here
 
 if __name__ == '__main__':
     app.run(debug=True)
