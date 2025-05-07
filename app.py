b0808e9206127f8afb71c5308bb74f8c8357f691
benjaminm206 <benjaminm206@nycstudents.net>
2025-05-07 09:25:06 -0400

    5-7

--- app.py
+++ app.py
@@ -6,19 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Get all trivia questions
-    response = requests.get("https://opentdb.com/api.php?amount=10&type=multiple")
-    data = response.json()
+    with open("mobs.json") as f:
+        mobs = json.load(f)
+    return render_template(index.html, mobs = mobs)
 
-trivia_questions = []
-for item in data["results"]:
-    question = {
-        "question": html.unescape(item["question"]),
-        "question": html.unescape(item["correct_answer"]),
-        "incorrect_answers": [html.unescape(ans) for ans in item["incorrect_answer)"]],
-        "category": item["category"],
-        "difficulty": item["difficulty"]
-    }
-    trivia_questions.append(question)
 if __name__ == '__main__':
     app.run(debug=True)
+
+
