2d8a983b00fa1da34640792b3286e8184869992e
benjaminm206 <benjaminm206@nycstudents.net>
2025-05-12 09:23:49 -0400

    5-12

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
 from flask import Flask
 import requests
-import random
 
 app = Flask(__name__)
 
-verse_refs = [
-    "john 3:16",
-    "philippians 4:13",
-    "psalm 23:1",
-    "romans 8:28",
-    "isaiah 41:10",
-    "1 corinthians 13:4-7",
-    "joshua 1:9",
-    "proverbs 3:5-6",
-    "matthew 11:28",
-    "galatians 6:9"
-]
-
 @app.route("/")
 def index():
-    response = requests.get("https://https://bible-api.com/BOOK+CHAPTER:VERSE")
-    deta = response.json()
-    verse_list = data['results']
+    response = requests.get("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=xml")
+    data = response.json()
 
-verses = []
+weather = []
 
-for verse in verse_list:
-    url = verse['url']
+for weather in weather_list:
+    url = weather['url']
     parts = url.strip("/").split("/")
     id = parts[-1]
 
-    verse.append({
-        'text': verse['name'].capitalize(),
-        'id': id,
+    weather.append({
+        'temp': weather['temp'],
+        'wind-speed': weather['wind'],
+        'precipitation': weather['precipitation'],
+        'hourly-forcast': weather['hourly-forecast'],
+        'visibility': weather['visibility'],
+        'aqi': weather['air-quality-index'],
+        'uv': weather['uv-index']
     })
 
 """   return render_template("index.html, verses=verses") """
