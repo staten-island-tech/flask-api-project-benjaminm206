2dbe8f59fab005e9bcf8493de44d2608f3470e7a
benjaminm206 <benjaminm206@nycstudents.net>
2025-05-21 09:24:28 -0400

    5-21

--- app.py
+++ app.py
@@ -11,46 +11,32 @@ def home():
 def weather():
     forecast = None #Initializing an empty value to soon be filled
     city = None
-    condition = None
+    current_temp = None
+    hourly = None
+
     if request.method == "POST": #sending data to API
         city = request.form.get("city") #gets the city via the form on the index
         formatted_city = city.replace(" ", "+") #removes spaces for URL
         geocoding_url = f"https://nominatim.openstreetmap.org/search?q={formatted_city}&format=json" #takes the city and converts it into latitude and longitude
         response = requests.get(geocoding_url, headers={"User-Agent": "weather-app"})
-        location_data = response.json()[0]
+        location_data = response.json()[0] #Location data = the response the API returns back -> as a list (json)
         lat = location_data["lat"] #latitude
         lon = location_data["lon"] #longitude
 
-    if lat and lon:
-        points_url = f"https://api.weather.gov/points/{lat},{lon}"
-        points_response = requests.get(points_url).json()["properties"]
-    
+        points_url = f"https://api.weather.gov/points/{lat},{lon}" #Inputs lat and lon to the weather API
+        points_response = requests.get(points_url).json()["properties"] #Recieving the weather of the location based on lat and lon
+
         forecast_url = points_response["forecast"]
         hourly_url = points_response["forecastHourly"]
+        details_url = points_response["forecastGridData"]
 
         forecast = requests.get(forecast_url).json()
         hourly = requests.get(hourly_url).json()
+        details = requests.get(details_url).json()
 
         current_temp = forecast["properties"]["periods"][0]["temperature"]
 
-        condition = forecast['properties']['periods'][0]['shortForecast'].lower()
-
-    return render_template("weather.html", forecast=forecast, hourly=hourly, city=city, current_temp=current_temp)
-
-
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        forecast = response.json()
-        return render_template("weather.html", forecast=forecast)
-    except Exception as e:
-        print("Error:", e)
-        return render_template("weather.html", forecast=None)
+    return render_template("weather.html", forecast=forecast, hourly=hourly, details=details, city=city, current_temp=current_temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
