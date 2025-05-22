7173cf7227105af4928955bc0c6a44ba0c34f5f7
benjaminm206 <benjaminm206@nycstudents.net>
2025-05-22 09:07:28 -0400

    5-22

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def weather():
     city = None
     current_temp = None
     hourly = None
+    daily = None
 
     if request.method == "POST": #sending data to API
         city = request.form.get("city") #gets the city via the form on the index
@@ -25,15 +26,14 @@ def weather():
 
         points_url = f"https://api.weather.gov/points/{lat},{lon}" #Inputs lat and lon to the weather API
         points_response = requests.get(points_url).json()["properties"] #Recieving the weather of the location based on lat and lon
-
+        # Getting all the data
         forecast_url = points_response["forecast"]
         hourly_url = points_response["forecastHourly"]
         details_url = points_response["forecastGridData"]
-
+        # Converting it into a readable json
         forecast = requests.get(forecast_url).json()
         hourly = requests.get(hourly_url).json()
         details = requests.get(details_url).json()
-
         current_temp = forecast["properties"]["periods"][0]["temperature"]
 
     return render_template("weather.html", forecast=forecast, hourly=hourly, details=details, city=city, current_temp=current_temp)
